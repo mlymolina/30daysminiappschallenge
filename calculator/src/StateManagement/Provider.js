b11ddef0233eed3205c2f2727388ddd202c388a4
Emely <mly.molina.minaya@gmail.com>
2019-07-07 00:48:03 -0700

    added methods to get/modify result and history

--- calculator/src/StateManagement/Provider.js
+++ calculator/src/StateManagement/Provider.js
@@ -11,14 +11,49 @@ export default class CalculatorProvider extends React.Component {
   }
 
   onClick(id) {
-    console.log(id)
+    let output = this.removeNumberFormat(this.getOutput())
+    if (!isNaN(output) && !isNaN(id)) {
+      output += id
+      this.printOutput(output)
+    } 
+  }
+
+  getHistory() {
+    return this.state.history
+  }
+
+  printHistory(num) {
+    this.setState({
+      history: num
+    })
+  }
+
+  getOutput() {
+    return this.state.result
+  }
+
+  printOutput(num) {
+    if(num ===  "") {
+      this.setState({result: num})
+    } else {
+      this.setState({result: this.getFormattedNumber(num)})
+    }
+  }
+
+  getFormattedNumber(num) {
+    return Number(num).toLocaleString('en')
+  }
+  removeNumberFormat(num) {
+    return Number(num.replace(/,/g, ''))
   }
 
   render() {
     return(
       <CalculatorContext.Provider
         value={{
-          onBtnClick: this.onClick
+          history: this.state.history,
+          result: this.state.result,
+          onBtnClick: this.onClick.bind(this)
         }}
       >
         {this.props.children}
