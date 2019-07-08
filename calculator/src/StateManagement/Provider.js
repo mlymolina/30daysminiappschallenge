d4534536daccebfd442d9ed53a42cc2809fb172d
Emely <mly.molina.minaya@gmail.com>
2019-07-07 18:31:07 -0700

    basic caculator functionalities added

--- calculator/src/StateManagement/Provider.js
+++ calculator/src/StateManagement/Provider.js
@@ -6,15 +6,27 @@ export default class CalculatorProvider extends React.Component {
     super(props)
     this.state = {
       result: '',
-      history: ''
+      history: []
     }
   }
 
   onClick(id) {
     let output = this.removeNumberFormat(this.getOutput())
+    let history = this.state.history.slice(0);
+    let hLength = history.length;
     if (!isNaN(output) && !isNaN(id)) {
       output += id
       this.printOutput(output)
+    } else if (id === 'C') {
+      this.clear()
+    } else if (id  === 'CE') {
+      this.backspace()
+    } else if (id === '=') {
+      this.calculate()
+    } else if (isNaN(id) && this.state.result != '') {
+      this.printHistory(this.state.result, id)
+    } else if (isNaN(id) && isNaN(history[hLength - 1]) && this.state.result === '') {
+      this.changeOperator(id)
     } 
   }
 
@@ -22,13 +34,39 @@ export default class CalculatorProvider extends React.Component {
     return this.state.history
   }
 
-  printHistory(num) {
+  printHistory(num, operator) {
+    let newHistory = this.state.history.slice(0);
+    newHistory.push(num)
+    newHistory.push(operator)
+    this.printOutput("")
     this.setState({
-      history: num
+      history: newHistory
     })
   }
 
-  getOutput() {
+  clear(){
+    this.setState({
+      result: '',
+      history: []
+    })
+  }
+
+  backspace(){
+    let result = this.removeNumberFormat(this.state.result).toString();
+    result = result.substring(0, result.length - 1)
+    this.setState({
+      result: this.getFormattedNumber(result)
+    })
+  }
+
+  changeOperator(newOperator) {
+    let history = this.state.history.slice(0)
+    history[history.length - 1] = newOperator
+    this.setState({
+      history: history
+    })
+  }
+  getOutput(){
     return this.state.result
   }
 
@@ -45,6 +83,32 @@ export default class CalculatorProvider extends React.Component {
   }
   removeNumberFormat(num) {
     return Number(num.replace(/,/g, ''))
+  }
+
+  calculate() {
+    let history = this.state.history.slice(0)
+    let result = Number(this.removeNumberFormat(history[0]))
+    let last = this.state.result !== "" ? history.length : history.length - 1
+    for (let i = 1; i < last; i++) {
+      let num = i === last-1 ? this.removeNumberFormat(this.state.result) : 
+      Number(this.removeNumberFormat(history[i+1]))
+      if (history[i] === '+') {
+        result += num
+      } else if (history[i] === '-') {
+        result -= num
+      } else if (history[i] === '%') {
+        result = result % num
+      } else if (history[i] === '/') {
+        result = result / num
+      } else if (history[i] === '*') {
+        result = result * num
+      }
+    }
+    
+    this.setState({
+      history: [],
+      result: this.getFormattedNumber(result)
+    })
   }
 
   render() {
