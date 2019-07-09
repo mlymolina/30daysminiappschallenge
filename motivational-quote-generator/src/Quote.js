3c71c791831b6b416f88e96b9385cba1312ede67
Emely <mly.molina.minaya@gmail.com>
2019-07-08 17:51:23 -0700

    added tweet functionality

--- motivational-quote-generator/src/Quote.js
+++ motivational-quote-generator/src/Quote.js
@@ -10,9 +10,13 @@ export default class Quote extends React.Component{
         </div>
         <p>- {this.props.author}</p>
         <div id='btn'> 
-          <i className="fab fa-twitter"></i>
           <i className="fab fa-instagram"></i>
           <i className="far fa-save"></i>
+          <a className="twitter-share-button"
+            href={`https://twitter.com/intent/tweet?text=${this.props.quote}%20-${this.props.author}`}
+            data-size="large">
+            <i className="fab fa-twitter"></i>
+          </a>
         </div>
       </div>
     )
